1b5bef38c9f5cc163f2d801a552cddf20cc4cf29
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-05-07 11:09:59 -0700

    socket.io starting point

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const path          = require('path');
-const http          = require('http');
-const express       = require('express');
-const socketIO      = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
 
+const app = express();
+const PORT = 3000 || process.env.PORT;
+const server = http.createServer(app);
 
-//set our path to serve our HTML through the public folder 
-const publicPath    = path.join(__dirname, '/../public');
+// Set static folder
+app.use(express.static("public"));
 
-const port          = process.env.PORT || 3001;
-// call our express function
-let app             = express();
-//specify the http method to let HTTP connectio
-let server          = http.createServer(app);
-//socketIO connection
-let io              = socketIO(server);
+// Socket setup
+const io = socket(server);
+//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// Players array
+let users = [];
 
-app.use(express.static(publicPath));
+io.on("connection", (socket) => {
+  console.log("Made socket connection", socket.id);
 
+  socket.on("join", (data) => {
+    users.push(data);
+    io.sockets.emit("join", data);
+  });
 
-server.listen(port, ()=> {
-    console.log(`Server is up on port ${port}.`)
+  socket.on("joined", () => {
+    socket.emit("joined", users);
+  });
+
+  socket.on("rollDice", (data) => {
+    users[data.id].pos = data.pos;
+    const turn = data.num != 6 ? (data.id + 1) % users.length : data.id;
+    io.sockets.emit("rollDice", data, turn);
+  });
+
+  socket.on("restart", () => {
+    users = [];
+    io.sockets.emit("restart");
+  });
 });
-//open a socket.io connection 
-io.on('connection', (socket) => {
-	console.log('A user just connected to draft their Pokémon team.');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected from the draft.');
-    })
-});
 
-socket.on('startDraft', () => {
-	io.emit('startDraft');
-})
-
-socket.on('pokemonIsSelected', (data) => {
-    io.emit('pokemonIsSelected', data);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
